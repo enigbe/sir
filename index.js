61178f3407b4262c4d241269b9ab1082eab6a744
Enigbe Ochekliye <ochekliyeenigbe@gmail.com>
2021-08-18 22:37:25 +0100

    Add success.ejs and logic

--- index.js
+++ index.js
@@ -4,18 +4,9 @@ const { validationResult } = require('express-validator');
 const sendEmail = require('./email');
 const keys = require('./config/keys');
 
-const { validateEmailContent, validateEmailSubject, validateSenderEmail} = require('./validators');
+const { validateEmailContent, validateEmailSubject, validateSenderEmail, getError} = require('./validators');
 
 const app = express();
-
-const getError = (errors, prop) => {
-    try {
-        return errors.mapped()[prop].msg;
-    } catch(e) {
-        return '';
-    }
-
-};
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view_engine', 'ejs');
@@ -35,19 +26,17 @@ app.post(
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).render('error.ejs', {errors, getError});
-            console.log(errors);
         }
 
-        console.log(errors);
         const { sender_email, email_subject, email_content } = req.body;
 
         sendEmail(sender_email, email_subject, email_content, (error, data) => {
             if (error) {
-                console.log(error);
-                // res.status(500).json({ msg: 'Internal server error'});
+                // res.status(500).json({ msg: 'Error sending email'});
+                res.status(500).render('failure.ejs',{ msg: 'Error sending email'});
             } else {
-                console.log(data);
-                // res.status(200).json({ msg: 'Email sent'})
+                // res.status(200).json({ msg: 'Email sent successfully'})
+                res.status(200).render('success.ejs',{ msg: 'Email sent successfully'});
             }
         });
         
--- validators.js
+++ validators.js
@@ -1,9 +1,19 @@
 const { check } = require('express-validator');
 
+const getError = (errors, prop) => {
+    try {
+        return errors.mapped()[prop].msg;
+    } catch(e) {
+        return '';
+    }
+};
+
 module.exports = {
     validateSenderEmail: check('sender_email').trim().normalizeEmail().isEmail().withMessage('Invalid email'),
 
     validateEmailSubject: check('email_subject').trim().not().isEmpty().isString().isLength({min: 2, max: 50}).withMessage('Subject must be 2 - 50 characters long'),
 
-    validateEmailContent: check('email_content').trim().not().isEmpty().isString().isLength({min: 2, max: 250}).withMessage('Content length capped at 250 characters')
+    validateEmailContent: check('email_content').trim().not().isEmpty().isString().isLength({min: 2, max: 250}).withMessage('Content length capped at 250 characters'),
+
+    getError
 };
